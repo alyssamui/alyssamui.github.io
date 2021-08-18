aec8dc8f10b385eedc58f6fed6df0d48f275dfbd
Alyssa Mui <alyssamui23@gmail.com>
2021-08-18 02:30:34 -0400

    updated projects

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,10 +1,58 @@
 import "./portfolio.scss"
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import { useState } from "react";
 
-export default function portfolio() {
+export default function Portfolio() {
+    
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const data = [
+        {
+            id: "1",
+            title: "Drone Pose Plot Generator",
+            desc: "I developed a script in Python that takes in a bag file and reads the ROS2 message data. The script outputs a 3D plot based on the coordinate position of the drone during its flight.",
+            img: "assets/pose_plot.png",
+        },
+        {
+            id: "2",
+            title: "MATLAB Escape Room",
+            desc: "I worked with a team to develop an interactive virtual escape room in MATLAB. I had created the infrastructure that compiles all the puzzles into one cohesive program, and also modeled the 3D parts that are used in the puzzles.",
+            img: "img2",
+        },
+        {
+            id: "3",
+            title: "Title3",
+            desc: "desc3",
+            img: "img3",
+        },
+    ]
 
+    const handleClick = (dir)=>{
+        dir === 'left' ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2) : 
+        setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+    }
     return (
         <div className="portfolio" id="portfolio">
             <h1>Projects</h1>
+            <div className="slider" style={{transform:`translateX(-${currentSlide * 100}vw)`}}>
+                {data.map((d)=> (
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <h2> {d.title} </h2>
+                                <p>
+                                    {d.desc}
+                                </p>
+                            </div>
+                        </div>
+                        <div className="right">
+                            <img src={d.img} alt=""/>
+                        </div>
+                    </div>
+                </div>))}
+            </div>
+            <ArrowBackIosIcon fontSize='large' className="arrow left" alt="" onClick={()=>handleClick("left")}/>
+            <ArrowBackIosIcon fontSize='large' className="arrow right" alt=""onClick={()=>handleClick("right")}/>
         </div>
     )
 }
